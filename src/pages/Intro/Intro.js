cb87ffdfe5dfb37bc7610669de31945b88e53e5e
Robert Adams <adamsiii.robert@gmail.com>
2021-10-23 13:44:31 -0700

    created intro component and stylesheet

---
+++ src/pages/Intro/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Intro.css'
+
+export default function Intro() {
+    return (
+        <section id='intro'>
+            <h1>Hi, I'm Robert!</h1>
+
+            <p>
+                Welcome to my portfolio!
+            </p>
+        </section>
+    );
+}
