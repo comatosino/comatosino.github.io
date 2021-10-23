322df2010725bfb2ec42f2c34f61ddf269bdc32e
Robert Adams <adamsiii.robert@gmail.com>
2021-10-23 13:45:24 -0700

    created footer componenet and stylesheet

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="page-footer black my-footer">
+            <div className='container'>
+                © 2021 Robert Adams
+            </div>
+        </footer>
+    )
+}
