05f835e25b415c4bc224bb258f74dfa97dcb4ef2
Robert Adams <adamsiii.robert@gmail.com>
2021-10-23 13:43:40 -0700

    created projects folder and stylesheet

---
+++ src/pages/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Projects() {
+    return (
+        <section className='container'>
+            <h5>Recent Projects</h5>
+            <div className='divider'></div>
+
+            {/* render project cards here */}
+
+        </section>
+    )
+}
