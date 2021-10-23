02af2bc9b7f71d06dd8a6a16d6a35c2e46e0ddb8
Robert Adams <adamsiii.robert@gmail.com>
2021-10-23 13:45:04 -0700

    created carousel component and stylesheet

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Carousel.css';
+
+import Projects from '../../pages/Projects/Projects';
+import About from '../../pages/About/About';
+import Contact from '../../pages/Contact/Contact';
+
+export default function Carousel() {
+    return (
+        <div className="carousel carousel-slider center">
+            <div className="carousel-item" href="#one!">
+                <Projects />
+            </div>
+            <div className="carousel-item" href="#two!">
+                <About />
+            </div>
+            <div className="carousel-item" href="#three!">
+                <Contact />
+            </div>
+        </div>
+    )
+}
