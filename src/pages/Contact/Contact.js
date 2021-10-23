62f2165f7e98bf78e9c8b017b048ed88baec241a
Robert Adams <adamsiii.robert@gmail.com>
2021-10-23 13:46:38 -0700

    created contact component and stylesheet

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <section className='container'>
+            <h5>Contact</h5>
+            <div className='divider'></div>
+        </section>
+    )
+}
