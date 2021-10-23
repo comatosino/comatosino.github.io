e678e3d77bb868ad7a6b9908a23d6ff1cae1f298
Robert Adams <adamsiii.robert@gmail.com>
2021-10-23 13:46:23 -0700

    created about component and stylesheet

---
+++ src/pages/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <section className='container'>
+            <h5>About</h5>
+            <div className='divider'></div>
+        </section>
+    );
+}
