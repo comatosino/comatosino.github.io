e581341010049777f52ef33ab01a310a4102df13
Robert Adams <adamsiii.robert@gmail.com>
2021-10-23 13:45:46 -0700

    created header component and stylesheet

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+import Nav from '../Nav/Nav';
+
+export default function Header(props) {
+    return (
+        <header>
+            <Nav />
+        </header>
+    );
+}
