e354843ac7dc7c97c5182c2cf240fab81acf117f
Robert Adams <adamsiii.robert@gmail.com>
2021-10-24 09:55:03 -0700

    added content via props and added ProjectModal

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -5,23 +5,34 @@ import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import './ProjectCard.css';
+import ProjectModal from '../ProjectModal/ProjectModal';
 
-import test from '../../pages/Projects/webapp-screenshots/consolelog.png';
-
-export default function ProjectCard() {
+export default function ProjectCard(props) {
     return (
-        <div className="card my-card">
-            <div className="card-image hoverable">
-                <img src={test} alt='test'/>
-            </div>
+        <>
+            <div className="card my-card">
+                <div className="card-image hoverable">
+                    <img className='modal-trigger' data-target={props.id} src={props.imgsrc} alt='test'/>
+                </div>
 
-            <div className='card-stuff'>
-                <span className='card-title'>Card Title</span>
-                <div className='card-icons card-title'>
-                    <a href='!#' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-                    <a href='!#' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faExternalLinkAlt} /></a>
+                <div className='card-stuff'>
+                    <span className='card-title'>{props.name}</span>
+                    <div className='card-icons card-title'>
+                        <a href={props.repoUrl} target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+                        <a href={props.appUrl} target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faExternalLinkAlt} /></a>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <ProjectModal id={props.id}
+                          name={props.name}
+                          desc={props.desc}
+                          role={props.role}
+                          tech={props.tech}
+                          appUrl={props.appUrl}
+                          repoUrl={props.repoUrl}
+                          imgsrc={props.imgsrc}
+            />
+        </>
     )
 }
