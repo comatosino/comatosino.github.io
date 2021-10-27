a69def3bd32d65b43fb8af8da627a0cbd220e440
Robert Adams <adamsiii.robert@gmail.com>
2021-10-27 16:20:08 -0700

    modified copyright date to be programmatic

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
+    const thisYear = new Date().getFullYear();
+
     return (
-        <footer className="page-footer black my-footer">
-            <div className='container'>
-                © 2021 Robert Adams
+        <footer className="page-footer black my-footer z-depth-5">
+            <div id='copyright' className='container'>
+                © {thisYear} Robert Adams
             </div>
         </footer>
     )
