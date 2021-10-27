208e709d8b64864efa88960905967b7a940c0d77
Robert Adams <adamsiii.robert@gmail.com>
2021-10-27 16:18:13 -0700

    added ids to portfolio carousel pages

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,13 +9,13 @@ import Contact from '../../pages/Contact/Contact';
 export default function Carousel() {
     return (
         <section id='portfolio' className="carousel carousel-slider center">
-            <div className="carousel-item" href="#one!">
+            <div id='projects' className="carousel-item" href="#one!">
                 <Projects />
             </div>
-            <div className="carousel-item" href="#two!">
+            <div id='about' className="carousel-item" href="#two!">
                 <About />
             </div>
-            <div className="carousel-item" href="#three!">
+            <div id='contact' className="carousel-item" href="#three!">
                 <Contact />
             </div>
         </section>
