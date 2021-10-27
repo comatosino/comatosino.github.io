b738a3be84aa5305fc9d90773f50e41646bd6c40
Robert Adams <adamsiii.robert@gmail.com>
2021-10-27 16:31:44 -0700

    added useEffect to add event listeners to nav btns

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 import OverlayMenu from '../OverlayMenu/OverlayMenu';
@@ -8,14 +8,39 @@ import './Nav.css';
 export default function Nav() {
     const [menuActive, setMenuActive] = useState(false);
 
+    useEffect(() => {
+        const introPage = document.getElementById('intro');
+        const portfolio = document.getElementById('portfolio');
+        const overlay = document.getElementById('overlay-menu');
+        const initialsBtn = document.getElementById('show-intro');
+
+        const handleClick = () => {
+            introPage.classList.remove('hide-intro');
+            portfolio.classList.remove('show-carousel');
+            overlay.classList.remove('overlay-active');
+        }
+
+        initialsBtn.addEventListener('click', handleClick);
+
+        return () => {
+            initialsBtn.removeEventListener('click', handleClick);
+        }
+    }, [])
+
     const toggleActive = () => {
         setMenuActive(!menuActive);
     }
 
+    const menuCheck = () => {
+        if (menuActive) {
+            setMenuActive(!menuActive);
+        }
+    }
+
     return (
-        <nav className='black my-nav'>
+        <nav className='black my-nav z-depth-5'>
             <div className='container my-nav-container'>
-                <div className='initials-box'>
+                <div id='show-intro' className='initials-box' onClick={menuCheck}>
                     <div className='my-initials'>
                         RA
                     </div>
