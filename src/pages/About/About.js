bcea4a2557fe3ea5fe52211f82591cc58057a7f5
Robert Adams <adamsiii.robert@gmail.com>
2021-10-27 20:43:26 -0700

    finalized about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,22 +2,25 @@ import React from 'react';
 
 import Avatar from './images/avatar.jpg';
 
+import './About.css'
+
 export default function About() {
     return (
-        <section className='container'>
-            <h5>About</h5>
-            <div className='divider'></div>
+        <section className='about-container container'>
+            <h1 className='page-header'>About</h1>
+            <div className='divider z-depth-1'></div>
             <div className='section'>
                 <img src={Avatar} alt='avatar' className='circle' width='200px'/>
-                <p className="light left-align">
-                    Hello, I'm Robert!
-                </p>
-                <p className="light left-align">
-                    I'm a full-stack web developer who utilizes my applied physics and avionics background to implement agile solutions to complex problems.
-                </p>
-                <p className="light left-align">
-                    I'm an adaptive and communicative team member that emphasizes efficient and readable code. I’m eager to contribute my skills to a collaborative environment where I can further develop acquired skills as well as learn new ones!
-                </p>
+
+                <div className='about-content-container'>
+                    <p className="left-align">
+                        Hello, I'm Robert!
+                        <br/><br/>
+                        I'm a full-stack web developer who utilizes my applied physics and avionics background to implement agile solutions to complex problems.
+                        <br/><br/>
+                        I'm an adaptive and communicative team member that emphasizes efficient and readable code. I’m eager to contribute my skills to a collaborative environment where I can further develop acquired skills as well as learn new ones!
+                    </p>
+                </div>
             </div>
         </section>
     );
