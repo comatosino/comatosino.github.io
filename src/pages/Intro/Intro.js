2c27024b0c6760bf045c0aa804372cfdfe3a0fe7
Robert Adams <adamsiii.robert@gmail.com>
2021-10-27 20:41:20 -0700

    added intro page useEffect and elements

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './Intro.css'
 
 export default function Intro() {
+    useEffect(() => {
+        const introPage = document.getElementById('intro');
+        const portfolio = document.getElementById('portfolio');
+        const mainBtn = document.getElementById('portfolio-btn');
+
+        const handleClick = () => {
+            introPage.classList.add('hide-intro');
+            portfolio.classList.add('show-carousel');
+        }
+        mainBtn.addEventListener('click', handleClick);
+
+        return () => {
+            mainBtn.removeEventListener('click', handleClick);
+        }
+    }, []);
+
     return (
         <section id='intro'>
-            <h1>Hi, I'm Robert!</h1>
-
-            <p>
-                Welcome to my portfolio!
-            </p>
+            <div className='container'>
+                <h1 className='page-header light'>Hi, I'm Robert!</h1>
+                <div className='divider z-depth-1'></div>
+                <p className='flow-text'>
+                    I'm a web developer based in Seattle.
+                </p>
+                <p className='flow-text'>
+                    Welcome to my portfolio!
+                </p>
+                <button id='portfolio-btn' className='btn black z-depth-1'>
+                    View Portfolio
+                </button>
+            </div>
         </section>
     );
 }
