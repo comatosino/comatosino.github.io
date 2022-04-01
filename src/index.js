15366f8ee15e1d0a403b16ce7a3868b370d386c4
Robert Adams <adamsiii.robert@gmail.com>
2022-04-01 11:12:01 -0700

    import org

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "materialize-css/dist/css/materialize.min.css";
 
 import "./index.css";
 
