56ff90d3ac2031d8928aac43da558914874bf623
Robert Adams <adamsiii.robert@gmail.com>
2022-04-01 18:36:42 -0700

    added typewriter component

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,15 +1,30 @@
 import "./Intro.css";
 import Avatar from "./images/avatar.jpg";
-import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import React, { useState } from "react";
 
 const Intro = () => {
+  const [showCursor, setShowCursor] = useState(true);
+
   return (
     <section id="intro">
       <div className="container myContainer">
-        <img src={Avatar} alt="avatar" className="my-avatar circle" width="200px" />
+        <img
+          src={Avatar}
+          alt="avatar"
+          className="my-avatar circle"
+          width="200px"
+        />
         <h1 className="page-header light">Hi, I'm Robert!</h1>
         <div className="divider z-depth-1"></div>
-        <p className="flow-text">I'm a web developer based in Seattle.</p>
+        <p className="flow-text">
+          <Typewriter
+            words={[" I'm a web developer based in Seattle!"]}
+            cursor={showCursor}
+            cursorStyle="|"
+            onLoopDone={() => setTimeout(() => setShowCursor(false), 1000)}
+          />
+        </p>
         <p className="flow-text">Welcome to my portfolio!</p>
         <button id="portfolio-btn" className="btn black z-depth-1">
           View Portfolio
