e943a5684b6302146d073926ab18089268fa842e
Robert Adams <adamsiii.robert@gmail.com>
2022-04-01 18:36:05 -0700

    added props data to modal

--- src/components/ProjectModal/ProjectModal.js
+++ src/components/ProjectModal/ProjectModal.js
@@ -1,10 +1,6 @@
 import "./ProjectModal.css";
 import M from "materialize-css";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
@@ -22,31 +18,15 @@ const ProjectModal = (props) => {
     <div id={props.id} className="modal modal-close">
       <img className="responsive-img" src={props.imgsrc} alt="" />
       <div className="modal-content">
-        <div className="modal-title">
-          <h4>{props.name}</h4>
-          <div className="links">
-            <a href={props.appUrl} target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faExternalLinkAlt} />
-            </a>
-            <a href={props.repoUrl} target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-          </div>
-        </div>
-
-        <p className="">a description</p>
-
+        <h4>{props.name}</h4>
+        <p className="">{props.desc}</p>
         <div className="divider"></div>
-
         <h6>Role</h6>
-        <p>what i did</p>
-
+        <p>{props.role}</p>
         <div className="divider"></div>
-
-        <h6>tech</h6>
-        <div>
-          <p>what i used</p>
-        </div>
+        <h6>Tech</h6>
+        <p>{props.tech}</p>
+        <div className="divider"></div>
       </div>
     </div>,
     document.getElementById("modals")
